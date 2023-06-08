7ab831d098f887b8abc6db1435fb8adb2a11e825
emmzsjcet <125429034+emmanuelsjcet@users.noreply.github.com>
2023-06-08 12:13:01 +0530

    Add files via upload

---
+++ CO2/Search.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Search
+{
+public static void main(String[] args){
+System.out.println("Name:EMMANUEL A");
+System.out.println("Admsn_no:22MCA023");
+System.out.println("Course_code and Course_id:OOP Lab and 20MCA132");
+System.out.println("Date:25-04-23");
+int c, n, search, array[];
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the number of elements: ");
+n = in.nextInt();
+array = new int[n];
+System.out.println("Enter those " + n + " elements: ");
+for (c = 0; c < n; c++)
+array[c] = in.nextInt();
+System.out.println("Enter value to find: ");
+search = in.nextInt();
+for (c=0; c<n; c++)
+if(array[c] == search)
+{
+System.out.println(search + " is present at location " + (c+1));
+break;
+}
+if (c == n)
+System.out.println(search + "isn't presesnt in array.");
+}
+}
